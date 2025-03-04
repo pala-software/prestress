beadad68c7c2ccb68401bdf05c9056bc2da8da06
Jesse Sivonen <j.sivonen@hotmail.com>
2025-03-04 11:29:31 +0200

    Fix authentication

--- internal/server/authenticate.go
+++ internal/server/authenticate.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"net/http"
 	"net/url"
+	"strings"
 )
 
 const anonymousRole = "anonymous"
@@ -27,16 +28,23 @@ type authenticationResult struct {
 // If user can be authenticated from the request, pointer to result is returned.
 // Otherwise response is written and nil is returned.
 func (server Server) authenticate(writer http.ResponseWriter, request *http.Request) *authenticationResult {
-	token := request.Header.Get("Authorization")
-	if server.disableAuth || token == "" {
+	authorization := request.Header.Get("Authorization")
+	if server.disableAuth || authorization == "" {
 		return &authenticationResult{
 			UserId:   "",
 			RoleName: anonymousRole,
 		}
 	}
 
+	if !strings.HasPrefix(authorization, "Bearer ") {
+		writer.WriteHeader(500)
+		return nil
+	}
+
 	introspectionUrl := *server.introspectionUrl
-	introspectionUrl.Query().Add("token", token)
+	introspectionUrl.RawQuery = url.Values{
+		"token": []string{strings.TrimPrefix(authorization, "Bearer ")},
+	}.Encode()
 	introspectionUrl.User = url.UserPassword(server.clientId, server.clientSecret)
 	response, err := http.Get(introspectionUrl.String())
 	if err != nil {
