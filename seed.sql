27ebc0d19790dc5aca3c464b7d54e112530ef5c0
Jesse Sivonen <j.sivonen@hotmail.com>
2025-05-08 13:29:21 +0300

    Remove example code

--- seed.sql
+++ seed.sql
@@ -2,7 +2,3 @@ CREATE ROLE anonymous;
 CREATE ROLE authenticated;
 
 GRANT anonymous TO authenticated WITH INHERIT TRUE;
-
-CREATE TABLE document (id SERIAL PRIMARY KEY, body TEXT);
-GRANT ALL ON TABLE document TO anonymous;
-GRANT USAGE ON SEQUENCE document_id_seq TO anonymous;
